538871a61b905b1d7dc601ff8548c1a71db18655
Joey Kuhn <jkuhn@stevens.edu>
2018-10-17 16:20:12 -0400

    updated

--- frequency.js
+++ frequency.js
@@ -32,3 +32,39 @@ function check(ch){
         table[ch] = 1;
     }
 }
+
+$("#importFileBtn").change(function (event) {
+    $.each(event.target.files, function (index, infile) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            // e.target.result should contain the text
+            log(e.target.result);
+        };
+        var str = reader.readAsText(infile);
+    });
+});
+
+  /*function readTextFile(file:infile.dat)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}*/
+
+for (var i = 0; i < str.length; i++) {
+    if (isValid == true)
+    {
+        check(str.charAt(i));
+    }
+  }
