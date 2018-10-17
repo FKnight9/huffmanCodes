c0ab0238dd43ff59cb6b586afffe73b53617452f
Faris Ibrahim <faristheknight9@gmail.com>
2018-10-17 16:53:27 -0400

    removed string added data

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const frequency = require("./frequency");
 const fs = require('fs');
-let str = "";
+
 fs.readFile('infile.dat', 'utf8', function(err, data) {
 
     if (err) {
         console.log(err);
     } else {
-        frequency.getFrequencyTable(str)
+        frequency.getFrequencyTable(data)
     }
 });
+
+// 1. the frequency table for the source text,
+// 2. the Huffman code for each letter and digit in the source code, and
+// 3. the length of the coded message in terms of number of bits
